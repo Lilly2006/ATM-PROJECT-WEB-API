0c875b1b50f19a11e894ae6a5cdfcee32e740ae5
Lillyjeba Samuvel <lillyjebas@caresoftglobal.com>
2022-02-14 12:30:03 +0530

    Lilly added ATM project

---
+++ TransactionProject/TransactionProject/Controllers/CustomerController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TransactionProject.Model;
+
+namespace TransactionProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly TransactionDbContext atmdbcontext;
+        public CustomerController(TransactionDbContext _atmDbContext)
+        {
+            atmdbcontext = _atmDbContext;
+        }
+        [HttpGet]
+        public IEnumerable<CustomerDetails> GetCustomerDetails()
+        {
+            return atmdbcontext.customerDetails.ToList();
+        }
+        [HttpGet("GetAccountNumber")]
+        public CustomerDetails GetAccountNumber(long AccountNumber)
+        {
+            return atmdbcontext.customerDetails.Find(AccountNumber);
+        }
+        [HttpPost("InsertCustomer")]
+        public IActionResult InsertCargo([FromBody] CustomerDetails customerDetails)
+        {
+            if (customerDetails.AccountNumber.ToString() != "")
+            {
+                atmdbcontext.customerDetails.Add(customerDetails);
+                atmdbcontext.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPut("UpdateCustomer")]
+        public IActionResult UpdateCustomer([FromBody] CustomerDetails customerDetails)
+        {
+            if (customerDetails.AccountNumber.ToString() != "")
+            {
+                atmdbcontext.Entry(customerDetails).State = EntityState.Modified;
+                atmdbcontext.SaveChanges();
+                return Ok("Customer Details updated successfully!");
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("DeleteCustomer")]
+        public IActionResult DeleteCustomer(long AccountNumber)
+        {
+            var result = atmdbcontext.customerDetails.Find(AccountNumber);
+            atmdbcontext.customerDetails.Remove(result);
+            atmdbcontext.SaveChanges();
+            return Ok("Deleted Successfully");
+
+        }
+    }
+}
